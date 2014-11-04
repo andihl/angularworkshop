493272fc70879d4eca421b71808195e02d76e4f7
Andreas Ihling <andreas.ihling@asideas.de>
2014-11-04 09:40:31 +0100

    removed console.log

--- filters.js
+++ filters.js
@@ -4,8 +4,6 @@ angular.module("filters", [])
         return function(starList, query) {
             var matchingStars = [];
 
-            console.log(starList, query);
-
             angular.forEach(starList, function(star) {
                 if(star.data.name.indexOf(query) !== -1) {
                     matchingStars.push(star);
